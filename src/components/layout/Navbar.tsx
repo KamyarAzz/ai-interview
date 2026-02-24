4b3b332b9d5874f02aa23b26e4b0fee90d674c2b
Kamyar Azizi <kamyar2019azizi@gmail.com>
2026-02-24 12:04:35 +0100

    feat: added Navbar

---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <nav className="sticky top-0 px-6 py-4 shadow-lg flex justify-between">
+      <h1 className="text-2xl font-bold">AI Interview</h1>
+    </nav>
+  );
+}
--- src/components/layout/UserLayout.tsx
+++ src/components/layout/UserLayout.tsx
@@ -1,9 +1,13 @@
 import {Outlet} from "react-router";
+import Navbar from "./Navbar";
 
 export default function UserLayout() {
   return (
-    <div>
-      <Outlet />
+    <div className="flex flex-col w-full h-full relative">
+      <Navbar />
+      <div className="flex flex-col px-6 py-4 w-full h-full">
+        <Outlet />
+      </div>
     </div>
   );
 }
