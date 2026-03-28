0b7c8cb498d164c28a9836a9f2181667e5b40278
Kamyar Azizi <kamyar2019azizi@gmail.com>
2026-03-28 23:39:52 +0100

    chore: update profile component

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,14 +1,11 @@
 import {useAuthStore} from "@/stores/authStore";
-import {useEffect, useState} from "react";
+import {useState} from "react";
 import ProfileBox from "./ProfileBox";
 import ProfileImage from "./ProfileImage";
 
 export default function Profile() {
   const [isOpened, setIsOpened] = useState(false);
   const user = useAuthStore((s) => s.user);
-  useEffect(() => {
-    console.log(user);
-  }, [user]);
 
   return (
     <div className="w-10 h-10">
