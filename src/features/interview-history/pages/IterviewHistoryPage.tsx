afbb7fb001f069396a1a46dada609f984b259dde
Kamyar Azizi <kamyar2019azizi@gmail.com>
2026-03-30 00:11:35 +0200

    feat: Add placeholder pages for Dashboard, Interview History, and Settings

--- src/features/interview-history/pages/IterviewHistoryPage.tsx
+++ src/features/interview-history/pages/IterviewHistoryPage.tsx
@@ -1,6 +1,6 @@
 import Container from "@/components/ui/Container";
 
-export default function DashboardPage() {
+export default function IterviewHistoryPage() {
   return (
     <div className="flex flex-col w-full h-full">
       <Container className="w-full! h-full">Under Development</Container>
